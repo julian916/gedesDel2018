cc842f6674d9fcc714be2197555049b651eb61c2
julian916 <julianromero916@hotmail.com>
2018-06-16 20:17:19 -0300

    Modifique como se obtienen los hoteles para tener mas informacion en la visual

--- StoredProcedures/sp_HotelesComboBox.sql
+++ StoredProcedures/sp_HotelesComboBox.sql
@@ -5,5 +5,5 @@ IF OBJECT_ID('dbo.sp_HotelesComboBox', 'P') IS NOT NULL
 GO
 CREATE PROCEDURE dbo.sp_HotelesComboBox
 AS
-	SELECT id_Hotel, calle+'-'+CAST(nro_calle as varchar(10)) as dir_Hotel from CUATROGDD2018.Hoteles
+	SELECT id_Hotel, pais + '-' + ciudad + '-' + calle + '-' + CAST(nro_calle as varchar(10)) as dir_Hotel from CUATROGDD2018.Hoteles
 GO

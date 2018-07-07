1ada96be5361ff23665273e128108a3ab530c95d
angemendez89 <angemendez89@gmail.com>
2018-07-06 22:09:21 -0300

    Actualizo SP AltaUsuario

--- StoredProcedures/SP_altaUsuario.sql
+++ StoredProcedures/SP_altaUsuario.sql
@@ -5,20 +5,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-IF OBJECT_ID('CUATROGDD2018.SP_altaUsuario', 'P') IS NOT NULL
-    DROP PROCEDURE CUATROGDD2018.SP_altaUsuario
+IF OBJECT_ID('CUATROGDD2018.SP_AltaUsuario', 'P') IS NOT NULL
+    DROP PROCEDURE CUATROGDD2018.SP_AltaUsuario
 GO
 
-CREATE PROCEDURE [CUATROGDD2018].[SP_altaUsuario] @usuario varchar(255), @contras varchar(255),@emailUsu varchar (255),  @idRol int, @idHotel int,@tipoDNI nvarchar(50), @nroDNI numeric(18,0), @idUsu int out
+CREATE PROCEDURE [CUATROGDD2018].[SP_AltaUsuario] @usuario varchar(255), @contras varchar(255),@emailUsu varchar (255),  @idRol int, @idHotel int,@tipoDNI nvarchar(50), @nroDNI numeric(18,0), @idUsu int out
 AS
+Begin
 	INSERT INTO CUATROGDD2018.Usuarios (username, password)
 	SELECT @usuario,HASHBYTES('SHA2_256', @contras)
-	WHERE (NOT EXISTS (SELECT * FROM CUATROGDD2018.Usuarios WHERE username = @usuario))  
-		  AND (NOT EXISTS (SELECT * FROM CUATROGDD2018.Personas WHERE email = @emailUsu and tipo_documento=@tipoDNI and nro_documento=@nroDNI))
-	DECLARE @nuevoIDUsuario int
+	WHERE NOT EXISTS (SELECT 1 FROM CUATROGDD2018.Usuarios WHERE username = @usuario) 
+	  AND NOT EXISTS (SELECT 1 FROM CUATROGDD2018.Personas WHERE email = @emailUsu and tipo_documento=@tipoDNI and nro_documento=@nroDNI)
 	SELECT @idUsu=SCOPE_IDENTITY() FROM CUATROGDD2018.Usuarios
 	IF @idUsu is not null
 		BEGIN
 			INSERT INTO CUATROGDD2018.Usuario_X_Hotel_X_Rol (id_usuario,id_rol,id_hotel)
 			VALUES (@idUsu, @idRol,@idHotel)
 		END
+End

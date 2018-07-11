d701062906d89577c350099bb69bf547ada76286
angemendez89 <angemendez89@gmail.com>
2018-07-11 00:41:07 -0300

    Agrego SP que permite filtrar datos del hotel por varios campos

---
+++ StoredProcedures/SP_FiltrarHotelesPorCampos.sql
@@ -0,0 +1,35 @@
+CREATE PROCEDURE [CUATROGDD2018].[SP_FiltrarHotelesPorCampos]
+	@idUsu int,
+	@cantEstrellas numeric(18,0),
+	@ciudad nvarchar(255),
+	@pais nvarchar(255),
+	@nombre nvarchar(255)
+AS
+BEGIN
+
+	SELECT h.[id_hotel]
+		  ,h.[nombre]
+		  ,h.[telefono]
+		  ,h.[calle]
+		  ,h.[nro_calle]
+		  ,h.[ciudad]
+		  ,h.[pais]
+		  ,h.[cant_estrellas]
+		  ,h.[recarga_estrellas]
+		  ,h.[habilitado]
+		  ,h.[email]
+		  ,h.[fecha_creacion]
+	FROM [CUATROGDD2018].[Hoteles] h
+	INNER JOIN [CUATROGDD2018].[Usuario_X_Hotel_X_Rol] uxh 
+	ON h.id_hotel=uxh.id_hotel
+	INNER JOIN [CUATROGDD2018].[Roles] r
+	On uxh.id_rol=r.id_rol
+	WHERE uxh.id_usuario=@idUsu 
+	and r.nombre='Administrador'
+	and (h.ciudad = @ciudad OR @ciudad IS NULL OR @ciudad = '')
+	and (h.cant_estrellas = @cantEstrellas OR @cantEstrellas IS NULL OR @cantEstrellas = 0)
+	and (h.pais = @pais OR @pais IS NULL OR @pais = '')
+	and (h.pais = @nombre OR @nombre IS NULL OR @nombre = '')
+	
+END
+GO

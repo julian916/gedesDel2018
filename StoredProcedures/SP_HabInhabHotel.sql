0076f739bb003d408f064032711b59268099116e
angemendez89 <angemendez89@gmail.com>
2018-07-11 17:39:57 -0300

    Actualizo SP Habilitar/Inhabilitar Hotel

--- StoredProcedures/SP_HabInhabHotel.sql
+++ StoredProcedures/SP_HabInhabHotel.sql
@@ -1,36 +1,63 @@
 CREATE PROCEDURE [CUATROGDD2018].[SP_HabInhabHotel]
 (	
 	@id_hotel int,
+	@fechaInicio datetime,
+	@diasCierre int,
 	@comentario nvarchar(255)
 )
 AS
 BEGIN
-IF @comentario != NULL
-BEGIN
-		--Inhabilitar hotel
-		UPDATE [CUATROGDD2018].[Hoteles]
-		SET [habilitado]='False'
-		WHERE [id_hotel]=@id_hotel;
+	DECLARE @MENSAJE varchar(255),@fechaHasta date;
+	set @fechaHasta = @fechaInicio+@diasCierre;
 
-		INSERT [CUATROGDD2018].[Historial_Cierres](
-				[id_hotel],[fecha_desde],[detalle_cierre])
-		VALUES (@id_hotel,GETDATE(),@comentario);
-END
-ELSE
-BEGIN
-		--Habilitar hotel
-		UPDATE [CUATROGDD2018].[Hoteles]
-		SET [habilitado]='False'
-		FROM [CUATROGDD2018].[Hoteles] H
-		inner join [CUATROGDD2018].[Historial_Cierres] HC
-		on H.[id_hotel]=HC.[id_hotel] AND H.[id_hotel]=@id_hotel
-		WHERE [fecha_hasta] is null;
+	IF @comentario != NULL
+		BEGIN
+			IF EXISTS (SELECT 1 FROM [CUATROGDD2018].[Reservas] 
+					   WHERE (@fechaInicio BETWEEN [fecha_desde] AND [fecha_hasta]) 
+					   or (@fechaHasta BETWEEN [fecha_desde] AND [fecha_hasta]))
+			OR EXISTS (SELECT 1 FROM [CUATROGDD2018].[Estadias] 
+						WHERE (@fechaInicio BETWEEN [fecha_inicio] AND ([fecha_inicio]+[cant_noches]))
+						or (@fechaHasta BETWEEN [fecha_inicio] AND ([fecha_inicio]+[cant_noches])))
+			BEGIN
+				SET @MENSAJE='El hotel no se podrá cerrar entre las fechas '++@fechaInicio++' y '++@fechaHasta++'. Existen Reservas/Estadias en ese periodo'
+				SELECT @MENSAJE
+				RETURN
+			END
+			ELSE
+			BEGIN
+				--Inhabilitar hotel
+				UPDATE [CUATROGDD2018].[Hoteles]
+				SET [habilitado]='False'
+				WHERE [id_hotel]=@id_hotel;
 
-		UPDATE [CUATROGDD2018].[Historial_Cierres]
-		SET [fecha_hasta]=GETDATE()
-		FROM [CUATROGDD2018].[Historial_Cierres] HC
-		inner join [CUATROGDD2018].[Hoteles] H
-		on HC.[id_hotel]=H.[id_hotel] and HC.[id_hotel]=@id_hotel
-		WHERE [fecha_hasta] is null;
-END
+				INSERT [CUATROGDD2018].[Historial_Cierres](
+						[id_hotel],[fecha_desde],[detalle_cierre],[dias_cierre])
+				VALUES (@id_hotel,@fechaInicio,@comentario,@diasCierre)
+
+				SET @MENSAJE=''
+				SELECT @MENSAJE
+				RETURN
+			END
+		END
+	ELSE
+	BEGIN
+			--Habilitar hotel
+			UPDATE [CUATROGDD2018].[Hoteles]
+			SET [habilitado]='False'
+			FROM [CUATROGDD2018].[Hoteles] H
+			inner join [CUATROGDD2018].[Historial_Cierres] HC
+			on H.[id_hotel]=HC.[id_hotel] AND H.[id_hotel]=@id_hotel
+			WHERE [fecha_hasta] is null;
+
+			UPDATE [CUATROGDD2018].[Historial_Cierres]
+			SET [fecha_hasta]=GETDATE()
+			FROM [CUATROGDD2018].[Historial_Cierres] HC
+			inner join [CUATROGDD2018].[Hoteles] H
+			on HC.[id_hotel]=H.[id_hotel] and HC.[id_hotel]=@id_hotel
+			WHERE [fecha_hasta] is null;
+
+			SET @MENSAJE=''
+			SELECT @MENSAJE
+			RETURN
+	END
 END

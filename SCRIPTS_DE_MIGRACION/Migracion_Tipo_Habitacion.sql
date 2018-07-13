52667ab692419d2b0aeddb635c73969875a57fa1
gabiquichiz <gabrielaquichiz@gmail.com>
2018-07-13 10:57:25 -0300

    cammbio en script de migracion

--- SCRIPTS_DE_MIGRACION/Migracion_Tipo_Habitacion.sql
+++ SCRIPTS_DE_MIGRACION/Migracion_Tipo_Habitacion.sql
@@ -1,3 +1,10 @@
 INSERT INTO [CUATROGDD2018].[Tipo_Habitacion] ([id_tipo_habitacion],[descripcion],[porcentual])
 SELECT DISTINCT  Habitacion_Tipo_Codigo,Habitacion_Tipo_Descripcion,Habitacion_Tipo_Porcentual
 FROM gd_esquema.Maestra
+update CUATROGDD2018.Tipo_Habitacion 
+set
+    cant_Huespedes = (case	when descripcion='Base Simple' then 1
+							when descripcion='Base Doble' then 2
+							when descripcion='Base Triple' then 3
+							when descripcion='Base Cuadruple' then 4
+							else 5 end)

77f961b14c0a33ac4ffe95b86bee7f81c1ed67e3
angemendez89 <angemendez89@gmail.com>
2018-07-14 03:28:39 -0300

    Actualizo sp

--- StoredProcedures/SP_altaHabitacion.sql
+++ StoredProcedures/SP_altaHabitacion.sql
@@ -1,7 +1,16 @@
-CREATE PROCEDURE [CUATROGDD2018].[SP_AltaHabitacion] @idTipoHabitacion int, @idHotel int, @piso numeric(18, 0), 
- @frente nvarchar(50), @nroHab numeric(18, 0), @comodidades varchar(255)
-
-AS  
+IF OBJECT_ID('CUATROGDD2018.SP_AltaHabitacion') IS NOT NULL
+    DROP PROCEDURE CUATROGDD2018.SP_AltaHabitacion
+GO
+CREATE PROCEDURE [CUATROGDD2018].[SP_AltaHabitacion] (
+		@idTipoHabitacion int
+		,@idHotel int
+		,@piso numeric(18, 0)
+		,@frente nvarchar(50)
+		,@nroHab numeric(18, 0)
+		,@comodidades varchar(255)
+AS 
+BEGIN 
 	INSERT INTO [CUATROGDD2018].[Habitaciones] ( id_tipo_habitacion, id_hotel, piso, frente, nro_habitacion, comodidades)
 	VALUES ( @idTipoHabitacion, @idHotel, @piso, @frente, @nroHab, @comodidades)
+END
 GO

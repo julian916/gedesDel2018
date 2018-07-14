2adc1cdefabbc7c6fd19f74857720896e1396d68
angemendez89 <angemendez89@gmail.com>
2018-07-14 03:49:37 -0300

    Actualizo sp

--- StoredProcedures/SP_bloquerUsuario.sql
+++ StoredProcedures/SP_bloquerUsuario.sql
@@ -1,13 +1,13 @@
-USE GD1C2018
-GO
 IF OBJECT_ID('CUATROGDD2018.SP_bloquerUsuario', 'P') IS NOT NULL
     DROP PROCEDURE CUATROGDD2018.SP_bloquerUsuario
 GO
 CREATE PROCEDURE CUATROGDD2018.SP_bloquerUsuario @idUsuario int
 AS
+BEGIN
 	UPDATE CUATROGDD2018.Usuarios  
 	SET habilitado = 'False' 
 	WHERE id_usuario = @idUsuario;  
+END
 
 GO
 

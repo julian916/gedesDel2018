50bf9085a4dc1c7c7f9d1165cc8d7426bcd742c4
angemendez89 <angemendez89@gmail.com>
2018-07-14 03:47:49 -0300

    Actualizo sp

--- StoredProcedures/SP_altaUsuario.sql
+++ StoredProcedures/SP_altaUsuario.sql
@@ -1,10 +1,3 @@
-USE [GD1C2018]
-GO
-/****** Object:  StoredProcedure [CUATROGDD2018].[SP_AltaUsuario]    Script Date: 14/6/2018 22:29:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 IF OBJECT_ID('CUATROGDD2018.SP_AltaUsuario', 'P') IS NOT NULL
     DROP PROCEDURE CUATROGDD2018.SP_AltaUsuario
 GO
@@ -15,6 +8,8 @@ Begin
 	DECLARE @id int
 	INSERT INTO CUATROGDD2018.Usuarios (username, password)
 	VALUES (@usuario,@password)
-	SELECT @id=SCOPE_IDENTITY() FROM [CUATROGDD2018].[Usuarios]
+	SELECT @id=SCOPE_IDENTITY() 
+	FROM [CUATROGDD2018].[Usuarios]
 		 
 End
+GO

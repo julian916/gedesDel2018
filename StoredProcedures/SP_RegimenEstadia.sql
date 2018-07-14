baa59164fbe85b5c237f067a4c6aa5026ac7cadf
angemendez89 <angemendez89@gmail.com>
2018-07-14 14:32:42 -0300

    Actualizo sp

--- StoredProcedures/SP_RegimenEstadia.sql
+++ StoredProcedures/SP_RegimenEstadia.sql
@@ -5,5 +5,5 @@ CREATE PROCEDURE [CUATROGDD2018].[SP_RegimenEstadia]
 AS
 BEGIN
 	select id_regimen, descripcion 
-	from [GD1C2018].[Regimenes_Estadia]
+	from [CUATROGDD2018].[Regimenes_Estadia]
 END
